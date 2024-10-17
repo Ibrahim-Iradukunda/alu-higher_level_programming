fecf820cfc04f36285d9bf44c455fce2d7e989ef
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-10-17 19:01:43 +0200

    Play with strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-message = f"{str1} Battery Street! {str2}"
-print(message)
+str1 = "Welcome to Holberton School!"
+str2 = "Battery Street!"
+str1 = f"{str1} {str2}"
+print(str1)
