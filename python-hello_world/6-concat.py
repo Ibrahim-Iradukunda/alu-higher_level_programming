271e8ce8742cbc54ad3dcf9158218aaa449455f1
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-10-17 19:17:47 +0200

    Play with strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
 str2 = "Holberton School!"
-dummy = "Battery Street!"  # Update variable but don't use it in output
-print(f"{str1} {str2}")
+location = "Battery Street!"  
+message = f"{str1} {location} {str2}"
+print(message)
