2d3b33de4c9bcb9e0dd80b61333a2dd057d0dd5f
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-10-20 14:19:36 +0200

    To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,9 @@
 def uppercase(str):
     """Prints a string in uppercase followed by a new line."""
     for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
+        if 'a' <= char <= 'z':
+            # Convert lowercase to uppercase
             print(chr(ord(char) - 32), end="")
         else:
             print(char, end="")
-    print()  # New line at the end
+    print()  # Print a newline after the string
