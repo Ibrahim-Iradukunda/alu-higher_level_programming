0a5fec3b6de031198f375bcc158a1caf4f42aec0
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-10-29 11:34:54 +0200

    a function that prints the first x elements of a list and only integers

--- python-exceptions/2-main.py
+++ python-exceptions/2-main.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 safe_print_list_integers = __import__('2-safe_print_list_integers').safe_print_list_integers
 
-my_list = [1, 2, 3, 4, 5]
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, 5)  # since we can't use len(), using the exact number
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, 7)  # more than length of the list
+my_list = [1, 2, 3, 4]
+nb_print = safe_print_list_integers(my_list, len(my_list) + 4)
 print("nb_print: {:d}".format(nb_print))
