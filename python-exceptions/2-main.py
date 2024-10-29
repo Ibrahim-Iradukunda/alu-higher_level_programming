df4bc44ce7445820aa636c3a58d0f14d0205e6bf
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-10-29 12:12:58 +0200

    a function that prints the first x elements of a list and only integers

--- python-exceptions/2-main.py
+++ python-exceptions/2-main.py
@@ -6,12 +6,11 @@ nb_print = safe_print_list_integers(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
 
 my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, 5)  # since we can't use len(), using the exact number
+nb_print = safe_print_list_integers(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 
-nb_print = safe_print_list_integers(my_list, 7)  # more than length of the list
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, 4]
-nb_print = safe_print_list_integers(my_list, len(my_list) + 4)
-print("nb_print: {:d}".format(nb_print))
+try:
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
+except IndexError as e:
+    print("IndexError: list index out of range")
