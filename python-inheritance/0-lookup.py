c2b58cd4e1ae233aaf8ab2f2d296ae2990fc9960
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-05 13:35:27 +0200

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    
+    Args:
+        obj: The object to inspect.
+    
+    Returns:
+        List of attributes and methods of the object.
+    """
     return dir(obj)
--- python-inheritance/0.main.py
+++ python-inheritance/0.main.py
@@ -2,10 +2,13 @@
 lookup = __import__('0-lookup').lookup
 
 class MyClass1(object):
+    """Example class with no attributes."""
     pass
 
 class MyClass2(object):
+    """Example class with an attribute and method."""
     my_attr1 = 3
+    
     def my_meth(self):
         pass
 
