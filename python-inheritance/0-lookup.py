79a6f1adb2ff2499d54f00246300516e4ef345ca
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-05 13:59:47 +0200

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 0-lookup
+Contains a single function that returns the list of available attributes and methods of an object.
+"""
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
--- python-inheritance/0.main.py
+++ python-inheritance/0.main.py
@@ -12,6 +12,40 @@ class MyClass2(object):
     def my_meth(self):
         pass
 
-print(lookup(MyClass1))
-print(lookup(MyClass2))
+# Correct output - case: int
 print(lookup(int))
+
+# Correct output - case: float
+print(lookup(float))
+
+# Correct output - case: object
+print(lookup(object))
+
+# Correct output - case: list
+print(lookup(list))
+
+# Correct output - case: class with dir method
+class MyClass3():
+    def dir(self):
+        return ["my_class", "is", "empty"]
+
+print(lookup(MyClass3))
+
+# Correct output - case: class with no attributes or methods
+class MyClass4():
+    pass
+
+print(lookup(MyClass4))
+
+# Correct output - case: class with one attribute
+class MyClass5():
+    one_attribute = 89
+
+print(lookup(MyClass5))
+
+# Correct output - case: class with one method
+class MyClass6():
+    def one_meth(self):
+        pass
+
+print(lookup(MyClass6))
