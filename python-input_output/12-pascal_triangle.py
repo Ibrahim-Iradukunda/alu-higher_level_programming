964c7db95f155219a526b460f35de71c76fa0207
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-11 19:38:26 +0200

    pascal_triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]    
+    triangle = [[1]]
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
