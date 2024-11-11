a10aa8e866d7a7ef76d61449461f66506a95ba50
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-11 17:37:44 +0200

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,6 +7,7 @@ Defines a function that reads a text file (UTF8) and prints it to stdout.
 
 def read_file(filename=""):
     """
+
     Reads a text file (UTF8) and prints it to stdout.    
     Args:
         filename (str): The name of the file to read.
