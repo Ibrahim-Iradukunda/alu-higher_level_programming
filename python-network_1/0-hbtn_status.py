ab257ed54555fbe01d1a197d777a1c5dbb6fdedb
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-20 19:29:56 +0200

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This script fetches https://alu-intranet.hbtn.io/status
+This script fetches http://0.0.0.0:5050/status
 using urllib and displays the body of the response.
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'http://0.0.0.0:5050/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
