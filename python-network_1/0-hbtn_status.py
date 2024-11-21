741614816d379f440226d8132a7df1fef2d0450a
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-21 18:38:40 +0200

    What's my status?

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-This module fetches https://alu-intranet.hbtn.io/status using urllib.
-"""
-
-import urllib.request
-
-if __name__== "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-
-    
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
 
 
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
