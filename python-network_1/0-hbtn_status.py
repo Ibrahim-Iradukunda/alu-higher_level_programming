adf38cf2655c6acbacd2008d0ba50bc6036afa35
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-21 20:21:23 +0200

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,9 +8,12 @@ import urllib.request
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5050/status'
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except urllib.error.URLError as e:
+        print(e)
