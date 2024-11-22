de8f95ed12ef4768c66794645d78a790c62807d5
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-22 14:31:54 +0200

    What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-This script fetches https://intranet.hbtn.io/status
-using urllib and displays the body of the response.
-"""
-
+"""  fetches https://alu-intranet.hbtn.io/status  """
 import urllib.request
-import urllib.error
 
-if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print("<urlopen error {}>".format(e.reason))
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = "https://alu-intranet.hbtn.io/status"
+
+if __name__ == "__main__":
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode("utf-8"))
