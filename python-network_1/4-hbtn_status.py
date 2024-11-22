d7f6ab5bd53f4de1290df083e00390cef2b06fa2
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-22 20:46:58 +0200

    What's my status? #1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """
-This script fetches http://0.0.0.0:5050/status
+This script fetches the status from the provided URL
 using the requests package and displays the body of the response.
 """
 
 import requests
 
+def fetch_status(url):
+    try:
+        r = requests.get(url)
+        print("Body response:")
+        print("\t- type: {}".format(type(r.text)))
+        print("\t- content: {}".format(r.text))
+    except requests.RequestException as e:
+        print(f"<Request error: {e}>")
+
 if __name__ == "__main__":
-    r = requests.get("http://0.0.0.0:5050/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    urls = ["https://alu-intranet.hbtn.io/status", "http://0.0.0.0:5050/status"]
+    for url in urls:
+        print(f"Fetching from {url}")
+        fetch_status(url)
+        print()
+
