238d155ee792dff9025e5eb7d7d3e79e6700b077
Ibrahim-Iradukunda <i.ibrahim@alustudent>
2024-11-22 20:33:20 +0200

    What's my status? #1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
+"""__summary__
+- Write a Python script that
+- fetches https://intranet.hbtn.io/status.
 """
-Python script that fetches an URL with requests package
-"""
-
 import requests
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
-    
-    try:
-        response = requests.get(url)
-        # Check if the status code is OK (200)
-        if response.status_code == 200:
-            print("Body response:")
-            print("\t- type: {}".format(type(response.text)))
-            print("\t- content: {}".format(response.text.strip()))
-        else:
-            print("Failed to fetch the URL. Status code:", response.status_code)
-    except requests.RequestException as e:
-        print("Error during request:", e)
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
